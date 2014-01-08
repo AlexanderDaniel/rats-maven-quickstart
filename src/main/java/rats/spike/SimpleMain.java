f3c86b0c99aa7b1ead9bcb0256ed86d5fa706db1
Alexander Daniel <alexander.daniel@gmx.at>
2014-01-08 10:56:10 +0100

    Extracted parse method for unit tests

--- src/main/java/rats/spike/SimpleMain.java
+++ src/main/java/rats/spike/SimpleMain.java
@@ -3,12 +3,19 @@ package rats.spike;
 import java.io.*;
 
 class SimpleMain {
+
     public static void main(final String[] args) throws IOException {
-        String infix = args[0];
+        String postfix = parse(args[0]);
+        System.out.println(postfix);
+    }
+
+    public static String parse(String infix) throws IOException {
         Reader reader = new StringReader(infix);
         SimpleParser parser = new SimpleParser(reader, "args[0]");
         xtc.parser.Result result = parser.pprogram(0);
-        String postfix = (String) result.semanticValue();
-        System.out.println(postfix);
+        if (result.hasValue())
+            return result.semanticValue();
+        else
+            throw new RuntimeException(result.parseError().msg + " " + result.parseError().index);
     }
 }
